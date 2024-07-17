58921886b27851bff44d04b48342a5acb5739a58
EC2 Default User <ec2-user@ip-172-31-47-36.ap-northeast-2.compute.internal>
2024-07-17 08:57:05 +0000

    fix: m3u8 파일 생성 과정 수정. 브라우저에서 실행 안되던 문제 해결

--- cctv-process.py
+++ cctv-process.py
@@ -11,11 +11,11 @@ import pytz
 import numpy as np
 import requests
 from requests.auth import HTTPBasicAuth
+import math
 
 TEMP_OUTPUT_PATH = os.path.expanduser('~/codes/ts-processed')
 MODEL_PATHS = ['fire_seg_results.pt', 'v8_nano_results.pt']
-
-MAX_BULK_SIZE = 90 * 1024 * 1024  # 100MB
+MAX_BULK_SIZE = 90 * 1024 * 1024  # 90MB
 
 def get_timestamp():
     kst = pytz.timezone('Asia/Seoul')
@@ -93,64 +93,63 @@ def detect_and_save_video(video_path, output_path, tmp_path, models, target_clas
     
     es_index = 'industrial-cctv' if config['config_name'] == 'industrial_config' else 'office-cctv'
 
-    with ThreadPoolExecutor(max_workers=len(models)) as executor:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-            if frame_count % 2 == 0:  # Process every second frame
-                futures = []
-                for model in models:
-                    if 'fire_seg_results.pt' in model['path']:
-                        futures.append(executor.submit(predict_model, frame, model, target_classes, 0.3))
-                    else:
-                        futures.append(executor.submit(predict_model, frame, model, target_classes, 0.65))
-                        
-                frame_detections = []
-                frame_class_counts = {cls: 0 for cls in target_classes}
-                segmentation_masks = []
-                for i, future in enumerate(futures):
-                    outputs, class_counts, masks = future.result()
-                    frame_detections.extend(outputs)
-                    for cls, count in class_counts.items():
-                        frame_class_counts[cls] += count
-                    if masks:
-                        segmentation_masks.extend(masks)
-                previous_outputs = frame_detections
-                previous_class_counts = frame_class_counts
-
-                frame_timestamp = video_start_time + datetime.timedelta(seconds=(frame_count / fps))
-                detection_info = {
-                    'frame': json_frame_count,
-                    'timestamp': frame_timestamp.strftime('%Y-%m-%dT%H:%M:%S%z'),
-                    'detections': frame_class_counts
-                }
-                if config['config_name'] == 'industrial_config':
-                    detection_info['min_required_personnel'] = config['min_required_personnel']
-                
-                # Add to bulk data
-                bulk_data += json.dumps({"index": {"_index": es_index}}) + '\n'
-                bulk_data += json.dumps(detection_info) + '\n'
-
-                # Check if bulk_data size exceeds MAX_BULK_SIZE
-                if len(bulk_data.encode('utf-8')) > MAX_BULK_SIZE:
-                    send_bulk_to_elasticsearch(bulk_data, es_index)
-                    bulk_data = ''
-
-                json_frame_count += 1  # Increment JSON frame count
-            else:
-                frame_detections = previous_outputs
-                frame_class_counts = previous_class_counts
-
-            if frame_detections:
-                if 'fire_seg_results.pt' in models[0]['path']:
-                    draw_segmentation(frame, frame_detections, segmentation_masks)
+        if frame_count % 2 == 0:  # Process every second frame
+            futures = []
+            for model in models:
+                if 'fire_seg_results.pt' in model['path']:
+                    futures.append(predict_model(frame, model, target_classes, 0.3))
                 else:
-                    draw_bounding_boxes(frame, frame_detections)
+                    futures.append(predict_model(frame, model, target_classes, 0.55))
+                    
+            frame_detections = []
+            frame_class_counts = {cls: 0 for cls in target_classes}
+            segmentation_masks = []
+            for i, future in enumerate(futures):
+                outputs, class_counts, masks = future
+                frame_detections.extend(outputs)
+                for cls, count in class_counts.items():
+                    frame_class_counts[cls] += count
+                if masks:
+                    segmentation_masks.extend(masks)
+            previous_outputs = frame_detections
+            previous_class_counts = frame_class_counts
 
-            video_writer.write(frame)
-            frame_count += 1
+            frame_timestamp = video_start_time + datetime.timedelta(seconds=(frame_count / fps))
+            detection_info = {
+                'frame': json_frame_count,
+                'timestamp': frame_timestamp.strftime('%Y-%m-%dT%H:%M:%S%z'),
+                'detections': frame_class_counts
+            }
+            if config['config_name'] == 'industrial_config':
+                detection_info['min_required_personnel'] = config['min_required_personnel']
+            
+            # Add to bulk data
+            bulk_data += json.dumps({"index": {"_index": es_index}}) + '\n'
+            bulk_data += json.dumps(detection_info) + '\n'
+
+            # Check if bulk_data size exceeds MAX_BULK_SIZE
+            if len(bulk_data.encode('utf-8')) > MAX_BULK_SIZE:
+                send_bulk_to_elasticsearch(bulk_data, es_index)
+                bulk_data = ''
+
+            json_frame_count += 1  # Increment JSON frame count
+        else:
+            frame_detections = previous_outputs
+            frame_class_counts = previous_class_counts
+
+        if frame_detections:
+            if 'fire_seg_results.pt' in models[0]['path']:
+                draw_segmentation(frame, frame_detections, segmentation_masks)
+            else:
+                draw_bounding_boxes(frame, frame_detections)
+
+        video_writer.write(frame)
+        frame_count += 1
 
     # Send any remaining data in the buffer
     if bulk_data:
@@ -170,6 +169,10 @@ def detect_and_save_video(video_path, output_path, tmp_path, models, target_clas
     subprocess.run(convert_command, check=True)
     os.remove(temp_output_video_path)
 
+    # Use the calculated video length for EXTINF
+    ts_duration = video_length
+    config['extinf_max'] = update_m3u8(final_output_video_path, ts_duration, os.path.join(output_path, 'playlist.m3u8'), config['extinf_max'])
+
 def get_file_creation_time(file_path):
     creation_time = datetime.datetime.fromtimestamp(os.path.getctime(file_path))
     return creation_time.astimezone(pytz.timezone('Asia/Seoul'))
@@ -185,28 +188,42 @@ def maintain_max_files(directory, max_files):
     while len(files) > max_files:
         os.remove(files.pop(0))
 
-def update_m3u8(directory, m3u8_filename):
-    ts_files = [f for f in os.listdir(directory) if f.endswith('.ts')]
-    ts_files = sorted(ts_files, key=lambda f: os.path.getmtime(os.path.join(directory, f)))
+def update_m3u8(ts_file, ts_duration, m3u8_filename, extinf_max):
+    # Update the EXTINF max value
+    extinf_max = max(extinf_max, ts_duration)
+    target_duration = math.ceil(extinf_max)
 
-    playlist_header = [
-        '#EXTM3U',
-        '#EXT-X-VERSION:3',
-        '#EXT-X-TARGETDURATION:10',
-        '#EXT-X-MEDIA-SEQUENCE:0'
-    ]
+    # Extract the ts file name from the full path
+    ts_file_name = os.path.basename(ts_file)
 
-    playlist_segments = []
-    for ts_file in ts_files:
-        playlist_segments.append(f'#EXTINF:10,')
-        playlist_segments.append(ts_file)
+    # Read existing playlist
+    if os.path.exists(m3u8_filename):
+        with open(m3u8_filename, 'r') as f:
+            playlist_lines = f.readlines()
+    else:
+        playlist_lines = [
+            '#EXTM3U\n',
+            '#EXT-X-VERSION:3\n',
+            f'#EXT-X-TARGETDURATION:{target_duration}\n',
+            '#EXT-X-MEDIA-SEQUENCE:0\n'
+        ]
 
-    playlist_footer = ['#EXT-X-ENDLIST']
+    # Update TARGETDURATION in the header
+    for i, line in enumerate(playlist_lines):
+        if line.startswith('#EXT-X-TARGETDURATION'):
+            playlist_lines[i] = f'#EXT-X-TARGETDURATION:{target_duration}\n'
+            break
 
-    playlist_content = '\n'.join(playlist_header + playlist_segments + playlist_footer)
+    # Add the new segment
+    playlist_lines.append(f'#EXTINF:{ts_duration},\n')
+    playlist_lines.append(f'{ts_file_name}\n')
 
+    # Write updated playlist back to file
     with open(m3u8_filename, 'w') as f:
-        f.write(playlist_content)
+        f.writelines(playlist_lines)
+
+    return extinf_max
+
 
 def send_bulk_to_elasticsearch(bulk_data, es_index):
     url = f'https://ea86ace4539b432f84fc0f19c4c0c586.ap-northeast-2.aws.elastic-cloud.com/{es_index}/_bulk'
@@ -217,14 +234,13 @@ def send_bulk_to_elasticsearch(bulk_data, es_index):
 
     try:
         response = requests.post(url, headers=headers, data=bulk_data, auth=HTTPBasicAuth(username, password))
-        print("response:", response.status_code, response.text)
+        # print("response:", response.status_code, response.text)
     except requests.exceptions.SSLError as e:
         print("SSL error:", e)
     except requests.exceptions.ConnectionError as e:
         print("Error connecting to Elasticsearch:", e)
     except Exception as e:
         print("An error occurred:", e)
-
 
 def main(config_path):
     with open(config_path, 'r') as config_file:
@@ -239,6 +255,9 @@ def main(config_path):
 
     models = [load_model(model_path) for model_path in MODEL_PATHS]
 
+    # Initialize EXTINF max value
+    config['extinf_max'] = 0
+
     while True:
         next_file = get_next_file_to_process(process_ts_path)
         if next_file:
@@ -246,8 +265,7 @@ def main(config_path):
             detect_and_save_video(video_path, processed_ts_path, TEMP_OUTPUT_PATH, models, target_classes, config)
             os.remove(video_path)  # Remove the processed file
 
-            maintain_max_files(processed_ts_path, 15)
-            update_m3u8(processed_ts_path, m3u8_filename)
+            maintain_max_files(processed_ts_path, 30)
         else:
             time.sleep(5)  # Wait for a short while before checking again
 
